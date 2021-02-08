ca7757453f31900db36ef72a188043824bbc477b
Oisin Rood <oisinrood@gmail.com>
2021-02-08 16:20:18 +0000

    Day 22 Project: Pong (folder) | rm Pong root files

---
+++ day_22_pong/main.py
@@ -0,0 +1,95 @@
+import time
+from turtle import Screen
+from pong_paddle import Paddle
+from pong_ball import Ball
+from pong_scoreboard import Scoreboard
+
+
+def main():
+    global GAME_ON, FIRST_RUN
+
+    screen.listen()
+
+    while GAME_ON:
+
+        ball.move()
+        bounce()
+
+        if FIRST_RUN:  # Load scoreboard do not reload unless score change
+            scoreboard.display()
+            FIRST_RUN = False
+
+        if ball.not_in_play():
+            if ball.xcor() > 450:
+                score(1)
+            elif ball.xcor() < -450:
+                score(2)
+
+        screen.update()
+
+    screen.mainloop()
+
+
+def paddle_reset():
+    paddle_1.reset()
+    paddle_2.reset()
+    ball.reset()
+
+
+def bounce():
+
+    distance1 = paddle_1.distance(ball)
+    distance2 = paddle_2.distance(ball)
+
+    ball.bounce_x(distance1, distance2)
+
+
+def score(player=False):
+
+    if player == 1:
+        scoreboard.player1_score += 1
+        ball.MOVE_X = -10
+    else:
+        scoreboard.player2_score += 1
+        ball.MOVE_X = 10
+
+    scoreboard.display(player)
+    scoreboard.player_win(player)
+    time.sleep(2)
+    scoreboard.display()
+    ball.reset()
+
+
+FIRST_RUN = True
+GAME_ON = True
+SPEED = 20
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.tracer(0)
+screen.bgcolor("#222")  # Set background color
+screen.title("Pong Game")
+
+paddle_1 = Paddle(-356, 15)
+paddle_2 = Paddle(350, 15)
+ball = Ball(0, 15)
+scoreboard = Scoreboard()
+
+# screen.onkey(snake.up, "w")
+# screen.onkey(snake.down, "s")
+#
+
+screen.onkeyrelease(paddle_2.key_depressed, "Up")
+screen.onkeyrelease(paddle_2.key_depressed, "Down")
+screen.onkeypress(paddle_2.up, "Up")
+screen.onkeypress(paddle_2.down, "Down")
+screen.onkeyrelease(paddle_1.key_depressed, "w")
+screen.onkeyrelease(paddle_1.key_depressed, "s")
+screen.onkeypress(paddle_1.up, "w")
+screen.onkeypress(paddle_1.down, "s")
+
+screen.onkeypress(paddle_reset, "Home")
+
+screen.onkeypress(ball.move, "space")
+
+main()
---
+++ day_22_pong/pong_scoreboard.py
@@ -0,0 +1,62 @@
+from turtle import Turtle
+
+FONT = ("Courier New", 60, "bold")
+FONT2 = ("Consolas", 32, "bold")
+FONT3 = ("Wide Latin", 72, "bold")
+
+score_file_read = open("highscore", "r")
+try:
+    temp_score = score_file_read.read()  # read high score from save file
+    temp_score = temp_score.split(',')
+    for item in temp_score:
+        item = int(item)
+        print(type(item), item)
+except ValueError:
+    temp_score = [0, 0]
+score_file_read.close()
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.highscore = temp_score
+        self.player1_score = 0
+        self.player2_score = 0
+        self.penup()
+        self.color("#FFF")
+        self.hideturtle()
+        self.speed(-1)
+
+    def display(self, new_score=False):
+        self.clear()
+        self.goto(0, -40)
+        self.color("#333")
+        self.write(f"PONG", align="center", font=FONT3)
+        self.color("#FFF")
+        if new_score == 1:
+            self.color("#0F0")
+        self.goto(-100, 200)
+        self.write(f"{self.player1_score}", align="left", font=FONT)
+        self.color("#FFF")
+        if new_score == 2:
+            self.color("#0F0")
+        self.goto(100, 200)
+        self.write(f"{self.player2_score}", align="right", font=FONT)
+        self.color("#FFF")
+
+    def player_win(self, winner):
+        self.goto(0, 140)
+        self.color("#0F0")
+        self.write(f"PLAYER {winner}", align="center", font=FONT2)
+
+    def reset_score(self):
+        self.clear()
+        self.player1_score = 0
+        self.player2_score = 0
+        self.display()
+
+    def save_score(self):
+        if (self.player1_score > self.highscore[0]) or (self.player2_score > self.highscore[1]):
+            score_file = open("highscore", "w")
+            score_file.write(f"{self.player1_score},{self.player2_score}")
+            score_file.close()
